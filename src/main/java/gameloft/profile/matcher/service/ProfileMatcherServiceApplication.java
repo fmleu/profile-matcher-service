4916e78167aa1ae47ca9b3beef19fc489d8f02cb
Marius Leu <mariusleu@Mariuss-MacBook-Air.local>
2023-11-30 16:27:38 +0200

    first setup

--- src/main/java/gameloft/profile/matcher/service/ProfileMatcherServiceApplication.java
+++ src/main/java/gameloft/profile/matcher/service/ProfileMatcherServiceApplication.java
@@ -1,4 +1,4 @@
-package gameloft.profilematcherservice;
+package gameloft.profile.matcher.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class ProfileMatcherServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileMatcherServiceApplication.class, args);
 	}
-
 }
